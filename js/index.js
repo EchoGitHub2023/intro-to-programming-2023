e2b2cbac1aa23b060664ec3c1bd721e6c9104b96
Echo Wang <ewang77@ymail.com>
2023-11-04 13:37:47 -0700

    add dynamic year in copyright and list of skills via an array; DOM manipulation

---
+++ js/index.js
@@ -0,0 +1,19 @@
+let today = new Date();
+let thisYear = today.getFullYear();
+
+console.log(thisYear);
+
+yearDynamic = document.getElementById("copyright");
+yearDynamic.innerText = `copyright  ${thisYear}  Echo Wang`;
+
+
+let skillsArr = ["html","css","javascript"]
+let skillsList = document.getElementById("skills").querySelector("ul")
+    
+for( i=0 ; i<skillsArr.length ; i++ ){
+    let skillListItems = document.createElement("li");
+    skillListItems.innerText = skillsArr[i];
+    skillsList.appendChild(skillListItems);
+    console.log(skillsArr[i]);
+}
+
