709d52ea4e5af741ba1b5938165654c53c21048a
Echo Wang <ewang77@ymail.com>
2023-11-14 11:11:26 -0600

    add a form: 'Leave a Message': name, email, message, submit

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ yearDynamic.innerText = `copyright  ${thisYear}  Echo Wang`;
 let skillsArr = ["html","css","javascript"]
 let skillsList = document.getElementById("skills").querySelector("ul")
     
-for( i=0 ; i<skillsArr.length ; i++ ){
+for( let i=0 ; i<skillsArr.length ; i++ ){
     let skillListItems = document.createElement("li");
     skillListItems.innerText = skillsArr[i];
     skillsList.appendChild(skillListItems);
