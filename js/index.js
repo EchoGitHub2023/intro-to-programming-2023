4fc38c32cc0536666089550c9850de03ae85ebb4
Echo Wang <ewang77@ymail.com>
2023-12-10 20:37:46 -0600

    attempt(2) edit/save, change edit to save at click only. first attempt(clean) in comment

--- js/index.js
+++ js/index.js
@@ -20,10 +20,6 @@ for( let i=0 ; i<skillsArr.length ; i++ ){
 //  "leave_message" is what I will be adding things to, using DOM manipulation, so I select the form first.
 const messageForm = document.getElementById("message_form");
 
-
-// const messageList = document.getElementById("message_post").querySelector("ul");
-
-
 //  addEventListener() There is no "=" equal sign for the method.
 messageForm.addEventListener("submit", (eSubmitMessage) => {
     eSubmitMessage.preventDefault();
@@ -37,10 +33,11 @@ messageForm.addEventListener("submit", (eSubmitMessage) => {
     const messageList = document.getElementById("message_post").querySelector("ul");
     const newMessage = document.createElement("li");
     // display the messages in a list.
-    newMessage.innerHTML = `
-      <a href="mailto:${userEmail}">${userName}</a>
-      <span>wrote: ${userMessage}</span>`;
-      // console.log(messageList);
+
+    newMessage.innerHTML = 
+    `<a href="mailto:${userEmail}">${userName}</a>`
+    + `<span>wrote: ${userMessage}</span>`;
+
     //append newMessage<li> to messageList<ul>, one at a time, each append is a new <li>
     messageList.appendChild(newMessage);
 
@@ -55,53 +52,28 @@ messageForm.addEventListener("submit", (eSubmitMessage) => {
     editButton.type = 'button';
     editButton.innerText = 'Edit'
     newMessage.appendChild(editButton);
-    //  listen to "click" of editButton, change userMessage into a input-text field, get rid of the userMessage, change the editButton to SaveButton.
+    //  Click "Edit", change editButton to saveButton.
     editButton.addEventListener("click", (eEditingMessage) => {
-      // add a text field for editedMessage, make it starts with the value of userMessage.
-      const editingMessage = document.createElement("input");
-      editingMessage.type = "text";
-      editingMessage.value = `${userMessage}`
-      // put editedMessage before userMessage in the same node;
-      insertBefore(editingMessage, newMessage);
-      // get rid of the userMessage node, so what's left is the new node <editedMessage>.
-      removeChild(newMessage);
-      // put SaveButton before editButton;
-      insertBefore(saveButton, editButton);
-      // get rid of editButton node, so what's left is the safeButton node.
-      removeChild(editButton);
+      // define li: the parent node of edit Button.
+      li = editButton.parentNode;
+      // make "save" button.
+      const saveButton = document.createElement('button');
+      saveButton.type = 'button';
+      saveButton.innerText = 'Save'
+      // insert save before edit.
+      li.insertBefore(saveButton, editButton);
+      // get rid of edit button.
+      li.removeChild(editButton);
     })
+      
 
 
-
-    // make "save" button, append to each <li>
-    const saveButton = document.createElement('button');
-    saveButton.type = 'button';
-    saveButton.innerText = 'Save'
-    newMessage.appendChild(saveButton);
-    //  listen to "click" of saveButton, change the input-text field into a editedMessage span, get rid of the editingMessage, change the saveButton back to editButton.
-    editButton.addEventListener("click", (eSaveMessage) => {
-      // add a span to display edited message, the value of the message is editingMessage.
-      const editedMessage = document.createElement("span");
-      editedMessage.value = `${editingMessage}`
-      // put editedMessage before editingMessage in the same node;
-      insertBefore(editedMessage, editingMessage);
-      // get rid of the editingMessage node, so what's left is the new node <editedMessage>.
-      removeChild(editingMessage);
-      // put editButton before saveButton;
-      insertBefore(editButton, saveButton);
-      // get rid of saveButton node, so what's left is the editButton node.
-      removeChild(saveButton);
-    })
-
-
-
-
-    // make "remove" button.
+    // make "remove" button, append to each <li>
     const removeButton = document.createElement('button');
     removeButton.type = 'button';
     removeButton.innerText = 'Remove'
-    // append "remove" button to each message <li>
     newMessage.appendChild(removeButton);
+
     //  listen to "click" of Remove Button, remove the removeButton and its parentNode<li> at the same time.
     removeButton.addEventListener("click", (eRemoveMessage) => {
       const entry = removeButton.parentNode;
@@ -125,13 +97,72 @@ if(messageList2.hasChildNodes() === false){
 }
 
 
-// Try move some codes out of the function(addEventListener:eSubmitMessage) so it's easier to read.  Put most of the const stuff in the front, so they are available for the function(addEventListener) for RemoveButton, EditButton, and SaveButton.
-// After get the buttons out, seperate two addEventLIstener function to two individule functions, not one nested inside another.
-// eSubmitMessage vs eShowMessages
-// Assign an id to the submit button in html?  Need to select the whole form to grab three things in the form, not just the submit button.
 
 
+
+
+
+// consider adding a button to <aside> to tuck away the floating window.
+// <aside> background color dark green, orange border.
+// make (shrink) button in js, createElement button, append it to <aside>.
+// Listen click(js), change (position: relative) in css (inline css? in js??), make (shrink) button disappear(js).
+// use a <style> tag to inject an actual string of CSS into the DOM.
+// if(button click){
+//   position: relative; (cs)
+//   (remove button) display: none; (cs)
+//   remove/hide button; (js)
+// }
+// The button is only needed on large screen.  Get rid of the button in @media.
 
       
    
+
+//     // make "edit" button.
+//     const editButton = document.createElement('button');
+//     editButton.type = 'button';
+//     editButton.innerText = 'Edit'
+//     // append "editButton" to each message <li>
+//     newMessage.appendChild(editButton);
+
+//     // make "save" button.
+//     const saveButton = document.createElement('button');
+//     saveButton.type = 'button';
+//     saveButton.innerText = 'Save'
+//     // append "saveButton" to each message <li>
+//     newMessage.appendChild(saveButton);
+
+// //  listen to "click" of editButton, change userMessage into a input-text field, get rid of the userMessage, change the editButton to SaveButton.
+// editButton.addEventListener("click", (eEditingMessage) => {
+//   // add a text field for editedMessage, make it starts with the value of userMessage.
+//   const editingMessage = document.createElement("input");
+//   editingMessage.type = "text";
+//   editingMessage.value = `${userMessage}`
+//   // put editedMessage before userMessage in the same node;
+//   insertBefore(editingMessage, userMessage);
+//   // get rid of the userMessage node, so what's left is the new node <editedMessage>.
+//   removeChild(userMessage);
+//   // put SaveButton before editButton;
+//   insertBefore(saveButton, editButton);
+//   // get rid of editButton node, so what's left is the safeButton node.
+//   removeChild(editButton);
+// })
+
+// //  listen to "click" of saveButton, change the input-text field into a editedMessage span, get rid of the editingMessage, change the saveButton back to editButton.
+// editButton.addEventListener("click", (eSaveMessage) => {
+//   // add a span to display edited message, the value of the message is editingMessage.
+//   const editedMessage = document.createElement("span");
+//   editedMessage.value = `${editingMessage}`
+//   // put editedMessage before editingMessage in the same node;
+//   insertBefore(editedMessage, editingMessage);
+//   // get rid of the editingMessage node, so what's left is the new node <editedMessage>.
+//   removeChild(editingMessage);
+//   // put editButton before saveButton;
+//   insertBefore(editButton, saveButton);
+//   // get rid of saveButton node, so what's left is the editButton node.
+//   removeChild(saveButton);
+// })
+
+// Try append both buttons save and edit, then make one of them hidden.
+
+
 
