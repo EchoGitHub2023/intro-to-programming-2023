c4944afcde1ad4e295d9d2424cad1d8cc2284be6
Echo Wang <ewang77@ymail.com>
2023-12-17 15:36:36 -0600

    nest save inside edit inside submit. can't get rid of the original message

--- js/index.js
+++ js/index.js
@@ -20,9 +20,12 @@ for( let i=0 ; i<skillsArr.length ; i++ ){
 //  "leave_message" is what I will be adding things to, using DOM manipulation, so I select the form first.
 const messageForm = document.getElementById("message_form");
 
+
+
 //  addEventListener() There is no "=" equal sign for the method.
 messageForm.addEventListener("submit", (eSubmitMessage) => {
     eSubmitMessage.preventDefault();
+
     // grab name, email, message from whatever the user typed into their browser.
     // use (.value) to grab the value of each field.
     const userName = eSubmitMessage.target.userName.value;
@@ -32,8 +35,8 @@ messageForm.addEventListener("submit", (eSubmitMessage) => {
     // select the empty <ul></ul> under "Messages" of html; and then create & add <li> into that <ul>
     const messageList = document.getElementById("message_post").querySelector("ul");
     const newMessage = document.createElement("li");
+    
     // display the messages in a list.
-
     newMessage.innerHTML = 
     `<a href="mailto:${userEmail}">${userName}</a>`
     + `<span>wrote: ${userMessage}</span>`;
@@ -45,35 +48,63 @@ messageForm.addEventListener("submit", (eSubmitMessage) => {
     // the .reset function is for form.  I need to select <form>, not <section>, for it to work.
     messageForm.reset();
 
-
-    
     // make "edit" button, append to each <li>
     const editButton = document.createElement('button');
     editButton.type = 'button';
     editButton.innerText = 'Edit'
     newMessage.appendChild(editButton);
-    //  Click "Edit", change editButton to saveButton.
+    //  listen to "click" of editButton, change userMessage into a input-text field, get rid of the userMessage, change the editButton to SaveButton.
     editButton.addEventListener("click", (eEditingMessage) => {
       // define li: the parent node of edit Button.
-      li = editButton.parentNode;
-      // make "save" button.
+      const li = editButton.parentNode;
+      // add a text field for editedMessage, make it starts with the value of userMessage.
+      const editingMessage = document.createElement("input");
+      editingMessage.type = "text";
+      editingMessage.value = `${userMessage}`
+      li.appendChild(editingMessage);
+      // put editedMessage before userMessage in the same node;
+      li.insertBefore(editingMessage, editButton);
+      // make "save" button. This save button is part of edit-click-eventListener.
       const saveButton = document.createElement('button');
       saveButton.type = 'button';
       saveButton.innerText = 'Save'
       // insert save before edit.
       li.insertBefore(saveButton, editButton);
-      // get rid of edit button.
-      li.removeChild(editButton);
-    })
-      
+      // hide edit button.  Use hidden instead of removeChild because I'll need edit button again later.
+      editButton.hidden = true;
+      // get rid of edit button
+      // li.removeChild(editButton);
 
+      // This "save" eventListener is nested inside "edit" eventListener, because this save-button is part of edit-click-eventListener.
+      // Listen to "save", make "input" become "span", change save to edit, get rid of input-edit field
+      saveButton.addEventListener("click", (eSavingMessage) => {
+        // define li2: the parent node of save Button newMessage<li>
+        const li2 = saveButton.parentNode;
+        // add a span, display the edited message from the edit-input field
+        const savingMessage = document.createElement("span");
+        savingMessage.innerHTML = 
+        `<a href="mailto:${userEmail}">${userName}</a>
+        <span>wrote: ${editingMessage.value} (edited)</span>`;
+        // put savingMessage before save Button
+        li2.insertBefore(savingMessage, saveButton);
+        // hide save button, un-hide edit button
+        saveButton.hidden = true;
+        editButton.hidden = false;
+        // git rid of input field
+        li2.removeChild(editingMessage);
+
+        // hide newMessage
+        // doesn't work: newMessage is the li(parent node) that everything else is appended on. 
+        newMessage.hidden = true;
+
+      }) // end of "save" eventListener
+    }) //end of "edit" eventListener
 
     // make "remove" button, append to each <li>
     const removeButton = document.createElement('button');
     removeButton.type = 'button';
     removeButton.innerText = 'Remove'
     newMessage.appendChild(removeButton);
-
     //  listen to "click" of Remove Button, remove the removeButton and its parentNode<li> at the same time.
     removeButton.addEventListener("click", (eRemoveMessage) => {
       const entry = removeButton.parentNode;
@@ -88,7 +119,7 @@ messageForm.addEventListener("submit", (eSubmitMessage) => {
       document.getElementById("message_post").hidden= false;
     }
 
-})
+}) //end of "submit" eventListener
 
 // without submit clickevent, the hidden is true. const another messageList(2) because it's outside submit click event, so it's less confusing.
 const messageList2 = document.getElementById("message_post").querySelector("ul");
